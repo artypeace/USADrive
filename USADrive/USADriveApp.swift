20f0598c6e5aee4ba5f7d4a02fdfd4ea8b90c60c
artypeace <1arty4u@gmail.com>
2024-02-17 14:58:12 +0700

    Initial Commit

---
+++ USADrive/USADriveApp.swift
@@ -0,0 +1,17 @@
+//
+//  USADriveApp.swift
+//  USADrive
+//
+//  Created by AV on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct USADriveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
