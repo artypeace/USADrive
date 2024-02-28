20d5260bdcc7722bb1a0d560bbd92c8c302b89b2
artypeace <1arty4u@gmail.com>
2024-02-28 17:52:41 +0700

    Same shit different day

---
+++ USADrive/NewMapView.swift
@@ -0,0 +1,18 @@
+//
+//  NewMapView.swift
+//  USADrive
+//
+//  Created by AV on 2/28/24.
+//
+
+import SwiftUI
+
+struct NewMapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NewMapView()
+}
