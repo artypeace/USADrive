27665677d14e7efdbe9936c6d8607b3a724bcf30
artypeace <1arty4u@gmail.com>
2024-03-18 20:51:33 +0700

    MainScreen created

---
+++ USADrive/USADriveCustomSheet/AppState.swift
@@ -0,0 +1,8 @@
+//
+//  AppState.swift
+//  USADrive
+//
+//  Created by AV on 3/18/24.
+//
+
+import Foundation
---
+++ USADrive/USADriveCustomSheet/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  USADrive
+//
+//  Created by AV on 3/18/24.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
